e64d96e7ecaac6eccbcf29011656b1629e956a4e
Ivan <ivan@plugable.com>
2015-01-22 13:42:03 -0800

    libbtaps.py is a library to easily interface with all functions of the Plugable PS-BTAPS1. btaps.py is a simple command line front-end to talk with the Plugable PS-BTAPS1 using libbtaps. This initial commit supports the following: Setting Switch On/Off. Reading current status of switch(name, on/off, timer settings). Creating, modifying and deleting timers. Changing the devices name Updating the devices date and time to the current date and time.

---
+++ btaps.py
@@ -0,0 +1,193 @@
+import sys
+import libbtaps
+import time
+
+
+def get_line():
+    line = raw_input('> ')
+    if line.lower() in ('quit', 'exit', 'kill'):
+        exit()
+    return line
+
+
+def print_timers(timer_list):
+    print "Timers:"
+    for timer in timer_list:
+        print "\tName: ", timer.name
+        print "\tID: ", timer.timer_id
+        print "\tOn: ",
+        if timer.on == 1:
+            print "On"
+        else:
+            print "Off"
+        print "\tDays: ", timer.repeat_days
+        print "\tStart Time: ", timer.start_time
+        print "\tEnd Time: ", timer.end_time
+        print ""
+
+
+def toggle_switch(btaps, status):
+    if status == 0:
+        btaps.set_switch(True)
+    else:
+        btaps.set_switch(False)
+
+
+def print_status(btaps):
+    name = btaps.get_dev_name()
+    status = btaps.get_switch_state()
+
+    print "Name: " + name
+    print "Switch: ",
+    if status[0] == 1:
+        print "On"
+    else:
+        print "Off"
+
+    return status
+
+
+def create_timer(btaps, timer_list):
+    print "Creating New Timer:"
+    print "Name: "
+    name = get_line()
+    new_timer = libbtaps.BTapsTimer(len(timer_list) + 1, name)
+
+    print "Enter Start and End Time in 24-hour format (ex: 23:54)"
+    print "Start Time: "
+    start = get_line()
+    start = time.strptime(start, "%H:%M")
+    new_timer.set_start_time(start[3], start[4])
+
+    print "End Time: "
+    end = get_line()
+    end = time.strptime(end, "%H:%M")
+    new_timer.set_end_time(end[3], end[4])
+
+    print "Repeat Timer?"
+    repeat = get_line().lower()
+    if repeat == "y":
+        day_list = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+        for i, day in enumerate(day_list):
+            print day, "?"
+            repeat = get_line().lower()
+            if repeat == 'y':
+                day_list[i] = True
+            else:
+                day_list[i] = False
+
+        new_timer.set_repeat_days(day_list[0], day_list[1], day_list[2], day_list[3],
+                                  day_list[4], day_list[5], day_list[6])
+
+    print "Enable New Timer? Y/N"
+    enable = get_line().lower()
+    if enable == 'y':
+        new_timer.toggle_on()
+
+    btaps.create_timer(new_timer)
+
+
+def modify_timer(btaps, timer_list):
+    print "Enter Timer ID for the timer you wish to modify:"
+    id = get_line()
+    mod_timer = timer_list[int(id)-1]
+
+    print "Enter values you wish to change, leave blank to keep original value"
+    print "Name: ", mod_timer.name
+    name = get_line()
+    if name != '':
+        mod_timer.set_name(name)
+
+    print "Enter Start and End Time in 24-hour format (ex: 23:54)"
+    print "Start Time: ", mod_timer.start_time
+    start = get_line()
+    if start != '':
+        start = time.strptime(start, "%H:%M")
+        mod_timer.set_start_time(start[3], start[4])
+
+    print "End Time: ", mod_timer.end_time
+    end = get_line()
+    if end != '':
+        end = time.strptime(end, "%H:%M")
+        mod_timer.set_end_time(end[3], end[4])
+
+    print "Repeat Timer?"
+    repeat = get_line().lower()
+    if repeat == "y":
+        day_list = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+        for i, day in enumerate(day_list):
+            print day, "?"
+            repeat = get_line().lower()
+            if repeat == 'y':
+                day_list[i] = True
+            else:
+                day_list[i] = False
+
+        mod_timer.set_repeat_days(day_list[0], day_list[1], day_list[2], day_list[3],
+                                  day_list[4], day_list[5], day_list[6])
+
+    print "Enable Timer? Y/N"
+    enable = get_line().lower()
+    if (enable == 'y') and (mod_timer.on != 1):
+        mod_timer.toggle_on()
+    elif (enable == 'n') and (mod_timer.on != 0):
+        mod_timer.toggle_on()
+
+    btaps.modify_timer(mod_timer)
+
+
+def main(argv):
+    print " === Plugable PS-BTAPS CLI v0.1 ==="
+    if len(argv) != 2:
+        print "USAGE: python " + sys.argv[0] + " [bdaddr]"
+        sys.exit(0)
+
+    btaps = libbtaps.BTaps(argv[1])
+    btaps.connect()
+    btaps.set_datetime_now()
+    status = print_status(btaps)
+    print_timers(status[1])
+
+    while True:
+        print "Select a function..."
+        print "1. (T)oggle Switch"
+        print "2. (C)reate Timer"
+        print "3. (M)odify Timer"
+        print "4. (D)elete Timer"
+        print "5. (S)et Device Name"
+        print "6. (G)et Switch Status (Name, On/Off, Timers)"
+        print "7. E(x)it"
+
+        try:
+            function = get_line().lower()
+            if function in ['1', 't']:
+                toggle_switch(btaps, status[0])
+            elif function in ['2', 'c']:
+                create_timer(btaps, status[1])
+            elif function in ['3', 'm']:
+                print_timers(status[1])
+                modify_timer(btaps, status[1])
+            elif function in ['4', 'd']:
+                print_timers(status[1])
+                print "Enter Timer ID to delete:"
+                timer_id = get_line()
+                btaps.delete_timer(timer_id)
+            elif function in ['5', 's']:
+                print "New Device Name:"
+                name = get_line()
+                btaps.set_dev_name(name)
+            elif function in ['6', 'g']:
+                status = print_status(btaps)
+                print_timers(status[1])
+            elif function in ['7', 'x']:
+                break
+
+            if not (function in ['5', 'g']):
+                status = print_status(btaps)
+        except KeyboardInterrupt:
+            break
+
+    btaps.disconnect()
+
+if __name__ == '__main__':
+    main(sys.argv)
---
+++ libbtaps.py
@@ -0,0 +1,256 @@
+from array import array
+import datetime
+
+import bluetooth
+
+
+def bad_hex_to_dec(hexa):
+    return (int(hexa/16) * 10) + hexa % 16
+
+
+class BTaps:
+    def __init__(self, btaddr):
+        self.socket = None
+        self.btaddr = btaddr
+
+    def __enter__(self):
+        self.connect()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.disconnect()
+
+    def __recv_data(self, length=128):
+        try:
+            data = self.socket.recv(length)
+            return data
+        except IOError as e:
+            print e
+            pass
+
+    def connect(self):
+        matches = bluetooth.find_service(address=self.btaddr)
+
+        if len(matches) == 0:
+            return 1
+
+        for match in matches:
+            if match['protocol'] == 'RFCOMM' and bluetooth.SERIAL_PORT_PROFILE in match['profiles']:
+                device = match
+                break
+        else:
+            return 1
+
+        self.socket = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+        self.socket.connect((device["host"], device["port"]))
+        return 0
+
+    def disconnect(self):
+        if self.socket:
+            self.socket.close()
+
+    def set_datetime_now(self):
+        now = datetime.datetime.now()
+        payload = buffer(array('B', [0xcc, 0xaa, 0x09, 0x09, 0x01,
+                                     int(now.strftime('%y'), 16), int(now.strftime('%m'), 16),
+                                     int(now.strftime('%d'), 16), int(now.strftime('%H'), 16),
+                                     int(now.strftime('%M'), 16), int(now.strftime('%S'), 16),
+                                     int(now.strftime('%w'), 16)]))
+
+        self.socket.send(payload)
+
+        response = self.__recv_data()
+        return response
+
+    def get_dev_name(self):
+        payload = buffer(array('B', [0xCC, 0xAA, 0x03, 0x18, 0x01, 0x19]))
+        self.socket.send(payload)
+        response = self.__recv_data()
+
+        return response[5:]
+
+    def set_dev_name(self, name):
+        if len(name) > 16:
+            return False
+
+        name_array = array('B', name)
+        packet = array('B', [0xCC, 0xAA, 0x12, 0x17, 0x01])
+        packet += name_array
+
+        if len(packet) > 21:
+            return False
+
+        while len(packet) < 21:
+            packet.append(0)
+
+        payload = buffer(packet)
+        self.socket.send(payload)
+        response = self.__recv_data()
+        return response
+
+    def set_switch(self, on):
+        if on:
+            # ON Payload = 0xCCAA03010101
+            payload = buffer(array('B', [0xCC, 0xAA, 0x03, 0x01, 0x01, 0x01]))
+        else:
+            # OFF Payload = 0xCCAA03010100
+            payload = buffer(array('B', [0xCC, 0xAA, 0x03, 0x01, 0x01, 0x00]))
+
+        self.socket.send(payload)
+        response = self.__recv_data()
+
+        response_bytes = memoryview(response).tolist()
+
+        if response_bytes == [0xCC, 0x55, 0x02, 0x01, 0x01]:
+            return False
+
+        # Other possible response_bytes
+        # ON:  cc:55:02:01:01:cc:55:03:02:01:01
+        # OFF: cc:55:02:01:01:cc:55:03:02:01:00
+
+        return True
+
+    def __set_timer(self, btapstimer, create=True):
+        name_array = array('B', btapstimer.name)
+
+        start_time = btapstimer.get_start_time_bad_hex()
+        end_time = btapstimer.get_end_time_bad_hex()
+        repeat_day = btapstimer.get_repeat_day_byte()
+
+        if create:
+            create_byte = 0x20
+        else:
+            create_byte = 0x03
+
+        packet = array('B', [0xCC, 0xAA, 0x1A, create_byte, 0x01, btapstimer.timer_id, repeat_day,
+                             start_time[0], start_time[1], end_time[0], end_time[1], btapstimer.on])
+        packet += name_array
+
+        if len(packet) > 29:
+            return False
+
+        while len(packet) < 29:
+            packet.append(0)
+
+        payload = buffer(packet)
+        self.socket.send(payload)
+        response = self.__recv_data()
+        return response
+
+    def create_timer(self, btapstimer):
+        self.__set_timer(btapstimer, True)
+
+    def modify_timer(self, btapstimer):
+        self.__set_timer(btapstimer, False)
+
+    def get_switch_state(self):
+        payload = buffer(array('B', [0xCC, 0xAA, 0x03, 0x12, 0x01, 0x13]))
+        self.socket.send(payload)
+
+        response = self.__recv_data()
+        on = memoryview(response).tolist()[7]
+        response_list = []
+
+        while True:
+            response = self.__recv_data(23)
+            if memoryview(response).tolist() == [0x00]:
+                break
+            else:
+                response_list.append(response)
+
+        timer_list = []
+        for timer_response in response_list:
+            timer_bytes = memoryview(timer_response).tolist()
+            timer = BTapsTimer(timer_bytes[0], timer_response[7:])
+            timer.set_repeat_days_byte(timer_bytes[1])
+            timer.set_start_time(bad_hex_to_dec(timer_bytes[2]), bad_hex_to_dec(timer_bytes[3]))
+            timer.set_end_time(bad_hex_to_dec(timer_bytes[4]), bad_hex_to_dec(timer_bytes[5]))
+            timer.on = timer_bytes[6]
+
+            timer_list.append(timer)
+
+        return on, timer_list
+
+    def delete_timer(self, timer):
+        if isinstance(timer, BTapsTimer):
+            timer_id = timer.timer_id
+        else:
+            timer_id = timer
+
+        payload = buffer(array('B', [0xCC, 0xAA, 0x04, 0x19, 0x01, 0x01, timer_id]))
+        self.socket.send(payload)
+
+        response = self.__recv_data()
+        return response
+
+
+class BTapsTimer:
+    def __init__(self, timer_id, name, start_time=None, end_time=None, on=False):
+        self.timer_id = None
+        self.set_id(timer_id)
+
+        self.name = None
+        self.set_name(name)
+
+        self.start_time = start_time
+        self.end_time = end_time
+        self.repeat_days = None
+        self.on = on
+
+    def set_name(self, name):
+        if len(name) <= 16:
+            self.name = name
+
+    def set_id(self, timer_id):
+        if timer_id < 255:
+            self.timer_id = timer_id
+
+    def set_repeat_days(self, mon=False, tue=False, wed=False, thu=False, fri=False, sat=False, sun=False):
+        self.repeat_days = {'Mon': mon, 'Tue': tue, 'Wed': wed, 'Thu': thu, 'Fri': fri, 'Sat': sat, 'Sun': sun}
+
+    def set_start_time(self, hour, minute):
+        self.start_time = datetime.time(hour, minute)
+
+    def set_end_time(self, hour, minute):
+        self.end_time = datetime.time(hour, minute)
+
+    def toggle_on(self):
+        self.on = not self.on
+
+    def get_start_time_bad_hex(self):
+        return int(self.start_time.strftime("%H"), 16), int(self.start_time.strftime("%M"), 16)
+
+    def get_end_time_bad_hex(self):
+        return int(self.end_time.strftime("%H"), 16), int(self.end_time.strftime("%M"), 16)
+
+    def get_repeat_day_byte(self):
+        if not self.repeat_days:
+            return 0
+
+        day_byte = 0
+        if self.repeat_days['Mon']:
+            day_byte ^= 0b00000010
+        if self.repeat_days['Tue']:
+            day_byte ^= 0b00000100
+        if self.repeat_days['Wed']:
+            day_byte ^= 0b00001000
+        if self.repeat_days['Thu']:
+            day_byte ^= 0b00010000
+        if self.repeat_days['Fri']:
+            day_byte ^= 0b00100000
+        if self.repeat_days['Sat']:
+            day_byte ^= 0b01000000
+        if self.repeat_days['Sun']:
+            day_byte ^= 0b10000000
+
+        return day_byte
+
+    def set_repeat_days_byte(self, byte):
+        day_list = [False] * 7
+        mon_byte = 0b00000010
+        for i, _ in enumerate(day_list):
+            day_byte = (mon_byte << i)
+            if (byte & day_byte) == day_byte:
+                day_list[i] = True
+
+        self.set_repeat_days(day_list[0], day_list[1], day_list[2], day_list[3], day_list[4], day_list[5], day_list[6])
